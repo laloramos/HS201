1657c1fc10cfe1d011fcd6a20f430aaa5478fc2c
Dane <dane.schilling@headspring.com>
2014-03-06 21:25:04 -0600

    CONFIGURED NHIBERNATE

---
+++ src/HS201_FinalAssignment/Data Access/NhConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using HS201_FinalAssignment.Entities;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Mapping.ByCode;
+
+namespace HS201_FinalAssignment.Data_Access
+{
+    public class NhConfiguration
+    {
+        private Configuration Configure()
+        {
+            var config = new Configuration();
+
+            config.SessionFactoryName("HS201");
+
+            config.DataBaseIntegration(x =>
+            {
+                x.Dialect<MsSql2008Dialect>();
+                x.Driver<SqlClientDriver>();
+                x.ConnectionStringName = "HS201_ConnectionString";
+                x.Timeout = 10;
+                x.BatchSize = 100;
+                x.KeywordsAutoImport = Hbm2DDLKeyWords.AutoQuote;
+                x.LogFormattedSql = true;
+            });
+
+            return config;
+        }
+
+        private HbmMapping GenerateMapping()
+        {
+            var map = new ModelMapper();
+
+            map.AddMappings(Assembly.GetAssembly(typeof (Conference)).GetExportedTypes());
+
+            return map.CompileMappingForAllExplicitlyAddedEntities();
+        }
+
+        public Configuration CreateConfiguration()
+        {
+            var configuration = Configure();
+            var mapping = GenerateMapping();
+
+            configuration.AddDeserializedMapping(mapping, "HS201");
+
+            return configuration;
+        }
+    }
+}
---
+++ src/HS201_FinalAssignment/Entities/Conference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HS201_FinalAssignment.Entities
+{
+    public class Conference
+    {
+
+    }
+}

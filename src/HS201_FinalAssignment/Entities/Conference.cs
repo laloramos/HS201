b63f5672b1c26e00c496000a868eb61f24d54091
Dane <dane.schilling@headspring.com>
2014-03-06 21:34:08 -0600

    ADDED CONFERENCE ENTITY AND MAPPING

--- src/HS201_FinalAssignment/Entities/Conference.cs
+++ src/HS201_FinalAssignment/Entities/Conference.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
 
 namespace HS201_FinalAssignment.Entities
 {
     public class Conference
     {
-
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
     }
 }
---
+++ src/HS201_FinalAssignment/Entities/ConferenceMapping.cs
@@ -0,0 +1,13 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+
+namespace HS201_FinalAssignment.Entities
+{
+    public class ConferenceMapping : ClassMapping<Conference>
+    {
+        public ConferenceMapping()
+        {
+            Id(x=>x.Id);
+            Property(x=>x.Name);
+        } 
+    }
+}
